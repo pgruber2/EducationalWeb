153322034b48e73860db79386ddd412db1e49c1c
Patrick Gruber <patrick@gruber.io>
2020-11-15 16:09:54 -0600

    add piazza info to ES

--- create_es_index.py
+++ create_es_index.py
@@ -1,6 +1,13 @@
+import os
+from bs4 import BeautifulSoup
+import json
 from elasticsearch import Elasticsearch
 from elasticsearch import helpers
 import io
+
+es = Elasticsearch([
+    {'host': '127.0.0.1', 'port': 9200}
+])
 
 
 def gendata(idx_dict):
@@ -13,8 +20,17 @@ def gendata(idx_dict):
         }
 
 
+def gendata_piazza(items):
+
+    for i, (post, nr) in enumerate(items):
+        yield {
+            "_index": "piazza",
+            "_id": i,
+            "_source": {"nr": nr, "content": post},
+        }
+
+
 def main():
-    es = Elasticsearch()
     cnt = []
     with io.open("./slides/slides.dat", "r", encoding="utf-8") as f:
         for l in f.readlines():
@@ -24,7 +40,18 @@ def main():
     with io.open("./slides/slides.dat.labels", "r", encoding="utf-8") as f:
         for l in f.readlines():
             lab.append(l[:-1])
-    print(len(lab), len(cnt))
+
+    # check slide count, if the index exists;
+    print("Slides", len(cnt), "Labels", len(lab))
+    try:
+        print(es.count(index="slides")["count"])
+        if es.count(index="slides")["count"] == len(lab):
+            print("Noting to do for slides. Moving on...")
+            return
+    except:
+        pass
+
+    # create the index;
     es.indices.create(
         index="slides",
         body={
@@ -36,8 +63,76 @@ def main():
             }
         },
     )
+    # add content;
     helpers.bulk(es, gendata(dict(zip(lab, cnt))))
+
+
+def parse_post(post):
+    posts = []
+
+    nr = post.get("nr", -1)
+    children = post.get("children", [])
+
+    if "history" in post:
+        post = post["history"][-1]
+
+    subject = BeautifulSoup(post.get("subject", ""), "lxml").text.strip().replace('\n', ' ').replace('\r', '')
+    content = BeautifulSoup(post.get("content", ""), "lxml").text.strip().replace('\n', ' ').replace('\r', '')
+
+    if subject:
+        posts.append(subject)
+    if content:
+        posts.append(content)
+
+    for child in children:
+        new_nr, new_posts = parse_post(child)
+        posts.extend(new_posts)
+        if new_nr > nr:
+            nr = new_nr
+
+    return nr, posts
+
+
+def main_piazza():
+    all_posts = []
+    all_nrs = []
+    for (dirpath, dirnames, filenames) in os.walk("./piazza/downloads"):
+        for file in filenames:
+            path = os.path.join(dirpath, file)
+            try:
+                with open(path, "r") as fh:
+                    nr, posts = parse_post(json.loads(fh.read()))
+                    nr = [nr for it in range(len(posts))]
+                    all_posts.extend(posts)
+                    all_nrs.extend(nr)
+            except (UnicodeDecodeError, json.decoder.JSONDecodeError):
+                continue
+
+    # check piazza count, if the index exists;
+    try:
+        print(es.count(index="piazza")["count"])
+        if es.count(index="piazza")["count"] == len(all_posts):
+            print("Noting to do for piazza. Moving on...")
+            return
+    except:
+        pass
+
+    # add piazza content;
+    if len(all_posts) > 0:
+        es.indices.create(
+            index="piazza",
+            body={
+                "mappings": {
+                    "properties": {
+                        "nr": {"type": "integer"},
+                        "content": {"type": "text", "analyzer": "english"},
+                    }
+                }
+            },
+        )
+        helpers.bulk(es, gendata_piazza(zip(all_posts, all_nrs)))
 
 
 if __name__ == "__main__":
     main()
+    main_piazza()
